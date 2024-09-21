f8ba023ef54b926e80e836e7daa73216fc34d29e
Sarahger <121996515+Sarahger@users.noreply.github.com>
2024-09-21 14:39:37 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:cooksavvy_app/screens/login_screen.dart';
+// import 'package:cooksavvy_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: LoginScreen(),
+    ),
+  );
+}
